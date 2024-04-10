bddddedf8a9cd86341ee5eddb93fb5c23380f3a4
misaka13210 <misaka13210@gmail.com>
2024-04-10 23:39:04 +0800

    更新

--- src/main/java/name/mahiro/mahiroclient.java
+++ src/main/java/name/mahiro/mahiroclient.java
@@ -2,7 +2,7 @@ package name.mahiro;
 
 import net.fabricmc.api.ClientModInitializer;
 
-;public class mahiroclient implements ClientModInitializer {
+public class mahiroclient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
