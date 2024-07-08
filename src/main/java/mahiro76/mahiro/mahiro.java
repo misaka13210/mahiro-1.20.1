0f6cee789a8252f3a3443bf0f34c57b16abcdd4a
misaka13210 <misaka13210@gmail.com>
2024-07-08 15:47:55 +0800

    初始提交

---
+++ src/main/java/mahiro76/mahiro/mahiro.java
@@ -0,0 +1,26 @@
+package mahiro76.mahiro;
+
+import mahiro76.mahiro.registry.ModItemGroup;
+import mahiro76.mahiro.registry.ModItems;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class mahiro implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "mahiro";
+    public static final Logger LOGGER = LoggerFactory.getLogger("mahiro");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello world!");
+		ModItemGroup.registerModItemGroup();
+		ModItems.registerModItems();
+	}
+}
